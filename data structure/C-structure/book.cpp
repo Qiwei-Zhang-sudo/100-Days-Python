52a2d7d069660a2f3250eee304a026d89189304c
zqw--sudu <2197134710@qq.com>
2026-04-06 17:53:31 +0800

    practice

--- data structure/C-structure/book.cpp
+++ data structure/C-structure/book.cpp
@@ -22,6 +22,7 @@ typedef struct
     int length;
 }Sqlist;
 
+//初始化
 Status InitList(Sqlist &L)
 {
     L.p = new Book[MAXSIZE];
@@ -34,6 +35,63 @@ Status InitList(Sqlist &L)
     return OK;
 }
 
+//获取元素
+Status GetElem(Sqlist L, int i, Book &e)
+{
+    if(i<1 || i>L.length)
+    {
+        cout<<"error"<<endl;
+        return ERROR;
+    }
+    e = L.p[i - 1];
+    return OK;
+}
+
+//获取位置
+int LocateElem(Sqlist L, float e)
+{
+    for (int i = 0; i < L.length; i++)
+    {
+        if(L.p[i].price == e)
+            return i + 1;
+    }
+    return 0;
+}
+
+//插入元素
+Status ListInsert(Sqlist &L,int i, Book e)
+{
+    if(L.length == MAXSIZE)
+    {
+        cout << "overflow" << endl;
+        return ERROR;
+    }
+        
+    if(i<1 || i>L.length + 1)
+    {
+        cout << "error" << endl;
+        return ERROR;
+    }
+    for (int j = L.length - 1; j >= i - 1; j--)
+        L.p[j + 1] = L.p[j];
+    L.p[i - 1] = e;
+    ++L.length;
+    return OK;
+}
+
+//删除元素
+Status ListDelete(Sqlist &L, int i)
+{
+    if (i<1 || i>L.length)
+    {
+        cout << "error" << endl;
+        return ERROR;
+    }
+    for (int j = i; j < L.length; j++)
+        L.p[j - 1] = L.p[j];
+    --L.length;
+    return OK;
+}
 int main()
 {
     cout<<"hello,world"<<endl;
